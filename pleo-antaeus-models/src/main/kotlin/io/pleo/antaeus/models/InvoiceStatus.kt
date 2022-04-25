73320066b3c069b10942b03112e6a595a72ad3ee
anis <anis.belkeir@gmail.com>
2022-04-24 21:53:16 -0400

    add more states Enum InvoiceStatus

--- pleo-antaeus-models/src/main/kotlin/io/pleo/antaeus/models/InvoiceStatus.kt
+++ pleo-antaeus-models/src/main/kotlin/io/pleo/antaeus/models/InvoiceStatus.kt
@@ -3,5 +3,10 @@ package io.pleo.antaeus.models
 enum class InvoiceStatus {
     PENDING,
     PAID,
-    FAILED
+    FAILED,
+    IN_PROGRESS,
+    ERROR_NETWORK,
+    ERROR_NOT_FOUND,
+    ERROR_CURRENCY
+
 }
